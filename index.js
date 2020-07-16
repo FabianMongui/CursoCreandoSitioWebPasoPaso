6bff4a5bd4d3996e8ce8eeb5b930938ad44cf43d
proyectos <proyectos@vone.com.co>
2020-07-15 23:37:37 -0500

    Mersh_final de la primera página

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     const lista_items = document.getElementById('lista-items');
-
+    configurarInicio();
     $('.myIcon').click(function(e){
         console.log(lista_items.className, 'click');
         
@@ -14,8 +14,20 @@ $(document).ready(function(){
             $('#navbar ul').addClass('display-none');
         }
     });
-
+    
     $('#content').click(function(){
-        $('#navbar ul').addClass('lista-sm');
+        $('#navbar ul').addClass('display-none');
     });
+
+    function configurarInicio(){
+        const urlPath = window.location.pathname;
+        $('nav a').each(function(){
+            var href = $(this).attr('href');
+            var indice = urlPath.length - href.length;
+            if(urlPath.substring(indice) === href){
+                console.log('match');
+                $(this).closest('li').addClass('active');
+            }
+        });
+    }
 });
